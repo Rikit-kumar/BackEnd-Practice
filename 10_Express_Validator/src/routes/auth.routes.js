b954793bafbab8c141feb926c419a94cc4b00cb9
Rikit-kumar <rishu143531@gmail.com>
2026-09-18 19:24:16 +0530

    replace manual validation with express-validator

--- 10_Express_Validator/src/routes/auth.routes.js
+++ 10_Express_Validator/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
 import { registerUserController } from '../controller/auth.controller.js';
+import { registerValidator } from '../validators/auth.validator.js';
 
 const router = Router();
 
-router.post('/register', registerUserController)
+router.post('/register', registerValidator ,registerUserController)
 
 export default router;
